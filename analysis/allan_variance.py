9aad8d56d89608f974579993e8fb3e56e14aef62
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2023-09-20 15:26:30 +0200

    Allan variance can also be computed on COSO counter values

--- analysis/allan_variance.py
+++ analysis/allan_variance.py
@@ -6,27 +6,27 @@ import sys
 # Test command line arguments
 if len(sys.argv)!=3:
 	print("Usage: python {:s} <data> <plot>".format(sys.argv[0]))
-	print("Generate a normalized Allan Variance <plot> for a ring oscillator. The <data> input file should contain one sample per period value at each line.")
+	print("Generate a normalized Allan Variance <plot> for a ring oscillator time serie or for COSO counter values. The <data> input file should contain one sample per line.")
 	quit()
 
 # Get parameters
 datafile = sys.argv[1]
 plotfile = sys.argv[2]
 
-# Load the ringo file
-periods = np.loadtxt(datafile)
+# Load the data file
+data = np.loadtxt(datafile)
 
-# Compute the absolute times and the average period
-ti = np.cumsum(periods)
-avg = np.mean(periods)
+# Compute the cumulative sum and the average value
+csum = np.cumsum(data)
+mean = np.mean(data)
 
-# Prepare a log space for ti size
-nspace = np.logspace(0, int(np.log10(np.size(ti)))-1, 1000).astype(int)
+# Prepare a log space for csum size
+nspace = np.logspace(0, int(np.log10(np.size(csum)))-1, 1000).astype(int)
 
 # Comput Allan variance
 allanvar = np.zeros(0)
 for n in nspace:
-	allanvar = np.append(allanvar, np.var(np.diff(np.diff(ti[0:np.size(ti):n])))/avg**2)
+	allanvar = np.append(allanvar, np.var(np.diff(np.diff(csum[0:np.size(csum):n])))/mean**2)
 
 # Do the polynomial regression LSNE (np.polyfit only fit values in high decades)
 poly = lsne.regression(nspace, allanvar, [2, 1, 0])
