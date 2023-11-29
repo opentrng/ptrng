fe9f86570f24cbbc6d82c7743bc2c47fd4de4094
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2023-11-29 18:42:36 +0100

    Added log option for distribution script

--- analysis/distribution.py
+++ analysis/distribution.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description="Generate a log distribution plot f
 parser.add_argument("datafile", type=str, help="data input file (text format, should contain one counter value per line)")
 parser.add_argument("plotfile", type=str, help="plot output file (possibles extensions png, jpg, pdf)")
 parser.add_argument("-t", "--title", type=str, default="", help="plot title")
+parser.add_argument("-l", "--log", action='store_true', help="set log scale for vertical axis")
 args=parser.parse_args()
 
 # Load the data
@@ -17,6 +18,7 @@ fig = plt.figure()
 plt.title(args.title)
 plt.xlabel('Values')
 plt.ylabel('Count')
-plt.yscale('log')
+if args.log:
+	plt.yscale('log')
 plt.hist(data, bins=100)
 plt.savefig(args.plotfile)
