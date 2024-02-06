6612d9cb6cd15101faa08ea3c27c490947d85a66
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2024-02-06 10:53:55 +0100

    Added script for remote frequency measurement

--- remote/fluart.py
+++ remote/fluart.py
@@ -1,7 +1,15 @@
 import serial
 
+# Try to load user settings or fallback to defaults
+try:
+	import settings
+	tty = settings.tty
+except:
+	tty = '/dev/ttyUSB1'
+
+# Define the UART interface for read and write operations
 class CmdProc:
-	def __init__(self, tty):
+	def __init__(self):
 		self.port = serial.Serial(tty, timeout=5)
 		self.port.baudrate = 115200
 
@@ -18,3 +26,7 @@ class CmdProc:
 		cmd += address.to_bytes(2, 'big')
 		cmd += value.to_bytes(4, 'big')
 		self.port.write(cmd)
+
+	def check(self, uid, rev):
+		assert uid==0xCEA3, "This is not an OpenTRNG target board"
+		assert rev==1, "Wrong version of OpenTRNG target board"
---
+++ remote/frequency.py
@@ -0,0 +1,38 @@
+import registers as Entropy
+import fluart as Fluart
+import argparse
+
+# Get command line arguments
+parser = argparse.ArgumentParser(description="Measure the frequency of a ring oscillator.")
+parser.add_argument("ro", type=int, help="index of the ring-oscillator (from 0 to 31)")
+args=parser.parse_args()
+assert args.ro in range(32)
+
+# Open the UART to the registermap
+interface = Fluart.CmdProc()
+reg = Entropy.RegMap(interface)
+
+# Check the board
+interface.check(reg.id_bf.uid, reg.id_bf.rev)
+
+# Enable the RO
+reg.ring_bf.enable = 1 << args.ro
+
+# Reset and enable the frequency counter
+reg.control_bf.reset = 1
+reg.freqcount_bf.en = 1
+
+# Select the ring and start the measurement
+reg.freqcount_bf.select = args.ro
+reg.freqcount_bf.start = 1
+
+# Wait until the measurement is done
+while reg.freqcount_bf.done==0:
+	True
+
+# Print the result
+print("Measured frequency for RO{:d}: {:f}MHz".format(args.ro, reg.freqcount_bf.result/10000))
+print("Overflow: {:}".format(reg.freqcount_bf.overflow==1 if True else False))
+
+# Disable the RO
+reg.ring_bf.enable = 0x00000000
