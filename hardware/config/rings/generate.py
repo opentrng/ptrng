45bc210024ca0334106aceb3c1dec5eba1393ac2
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2024-02-09 10:24:50 +0100

    Iterators are done for RO placement script

--- hardware/config/rings/generate.py
+++ hardware/config/rings/generate.py
@@ -3,42 +3,93 @@ import math
 
 # Get command line arguments
 parser = argparse.ArgumentParser(description="Generate the ring-oscillator configuration. ?????????????")
-parser.add_argument("-lut", required=True, type=int, help="number of LUT per slice")
-parser.add_argument("-x", required=True, type=int, help="rings placement zone, row of the first slice")
-parser.add_argument("-y", required=True, type=int, help="rings placement zone, column of the first slice")
-parser.add_argument("-width", required=True, type=int, help="rings placement zone width (in slice count)")
-parser.add_argument("-height", required=True, type=int, help="rings placement zone height (in slice count)")
+parser.add_argument("-luts", required=True, type=int, help="number of LUT per slice")
+parser.add_argument("-x", required=True, type=int, help="reserved area for rings placement, row of the first slice")
+parser.add_argument("-y", required=True, type=int, help="reserved area for rings placement, column of the first slice")
+parser.add_argument("-width", required=True, type=int, help="width of the reserved area for rings placement (in slice count)")
+parser.add_argument("-height", required=True, type=int, help="height of the reserved area for rings placement (in slice count)")
 parser.add_argument("-hpad", type=int, default=1, required=False, help="horizontal padding between ring-oscillators")
 parser.add_argument("-vpad", type=int, default=1, required=False, help="vertical padding between ring-oscillators")
 parser.add_argument("-len", type=int, nargs='+', help="number of elements in the ring-oscillator")
 args=parser.parse_args()
 
 # Command line argument summary
-print("Selected slices for ring: (X{:d},Y{:d}) to (X{:d},Y{:d})".format(args.x, args.y, args.x+args.width-1, args.y+args.height-1))
-print("LUTs per slice: {:d}".format(args.lut))
+print("Reserved area for rings: (X{:d},Y{:d}) to (X{:d},Y{:d})".format(args.x, args.y, args.x+args.width-1, args.y+args.height-1))
+print("LUTs per slice: {:d}".format(args.luts))
 print("Number of ring-oscillators: {:d} (T={:d})".format(len(args.len), len(args.len)-1))
 print("Lengths of ROs: [{:s}]".format(', '.join(map("{:d}".format, args.len))))
 
-# Slice iterator
-ro_index = 0
-ro_len = args.len[ro_index]
-ro_width = 2
-ro_height = math.ceil(ro_len/(ro_width*args.lut))
-ro_half = math.ceil(ro_len/ro_width)
-ro_x = 0
-ro_y = 0
-print("- RO_{:02d} len={:d} half={:d} x={:d} y={:d} width={:d} height={:d}".format(ro_index, ro_len, ro_half, ro_x, ro_y, ro_width, ro_height))
-for element in range(ro_len):
-	if element < ro_half:
-		i = 0
-		j = element
-		lut = j%args.lut
+# Add a reserved area for a frequency counter at given position (slice)
+def add_freqcounter(x, y):
+	width = 2
+	height = 1
+	print("- freqcounter_{:02d} origin=({:d},{:d}) size=({:d},{:d})".format(index, x, y, width, height))
+	return width, height
+
+# Add a reserved area for a clock divider at given position (slice)
+def add_clkdivider(x, y):
+	width = 2
+	height = 1
+	print("- clkdivider_{:02d} origin=({:d},{:d}) size=({:d},{:d})".format(index, x, y, width, height))
+	return width, height
+
+# Add a reserved area for a ring-oscillator at given position (slice)
+def add_ring(x, y, index, length):
+	width = 2
+	height = math.ceil(length/(width*args.luts))
+	half = math.ceil(length/height)
+	print("- RO_{:02d} len={:d} half={:d} origin=({:d},{:d}) size=({:d},{:d})".format(index, length, half, x, y, width, height))
+	for element in range(length):
+		if 2*element <= length:
+			i = 0
+			j = element
+			lut = j%args.luts
+		else:
+			i = 1
+			j = length-1-element
+		slice_i = i
+		slice_j = int(j/args.luts)
+		lut_i = j%args.luts
+		print("  - element={:d} slice=({:d},{:d}) lut={:d}".format(element, x+slice_i, y+slice_j, lut_i))
+	return width, height #, dict
+
+# Start at the lower left corner of the reserved area
+x = args.x
+y = args.y
+xstart = x
+ystart = y
+ymax = 0
+
+# For each RO
+for index in range(len(args.len)):
+
+	# Add the padding for isolation
+	x += args.hpad
+	y += args.vpad
+
+	# Add the frequency counter
+	width, height = add_freqcounter(x, y)
+	y += height
+
+	# Add the clock divider
+	width, height = add_clkdivider(x, y)
+	y += height
+
+	# Add the RO
+	width, height = add_ring(x, y, index, args.len[index])
+	y += height
+	x += 2
+
+	# Check if it fits the reserved area
+	assert x+args.hpad <= args.x+args.width, "Placement error for ring-oscillator {:d}, does not fit in the reserved width".format(index)
+	assert y+args.vpad <= args.y+args.height, "Placement error for ring-oscillator {:d}, does not fit in the reserved height".format(index)
+	ymax = max(y, ymax)
+
+	# Check if the next RO will fit in the remaining width in the current row, or start a new row
+	if x+args.hpad+2+args.hpad > args.x+args.width:
+		x = xstart
+		y = ymax
+		ystart = ymax
+		ymax = 0
 	else:
-		i = 1
-		j = ro_len-1-element
-	slice_i = i
-	slice_j = int(j/args.lut)
-	lut_i = j%args.lut
-	slice_xy = "X{:d}Y{:d}".format(ro_x+slice_i, ro_y+slice_j)
-	print("  - element={:d} slice={:s} lut={:d}".format(element, slice_xy, lut_i))
-	
+		y = ystart
