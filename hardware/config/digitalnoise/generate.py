7248f28397294db254228ad3f71cbd6f0bd4b90d
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2024-03-19 10:58:28 +0100

    New pblock hierarchy for digitalnoise constraints generator

--- hardware/config/digitalnoise/generate.py
+++ hardware/config/digitalnoise/generate.py
@@ -8,15 +8,15 @@ parser = argparse.ArgumentParser(description="Generate configuration files for t
 parser.add_argument("-vendor", required=True, type=str, choices=['xilinx'], help="target vendor (for selecting the templates)")
 parser.add_argument("-luts", required=True, type=int, help="number of LUT per item (per slice for Xilinx, per LE for Intel Altera)")
 parser.add_argument("-fixedlutpin", required=False, type=str, default='', help="optional parameter to fix the input pin for all LUTs (the value is the name of the pin)")
-parser.add_argument("-x", required=True, type=int, help="start row for the reserved area ")
-parser.add_argument("-y", required=True, type=int, help="start comlumn for the reserved area")
+parser.add_argument("-x", required=True, type=int, help="origin abscissa for the reserved area ")
+parser.add_argument("-y", required=True, type=int, help="origin ordinate for the reserved area")
 parser.add_argument("-maxwidth", required=True, type=int, help="maximum width for the reserved area")
 parser.add_argument("-maxheight", required=True, type=int, help="maximum height for the reserved area")
-parser.add_argument("-border", type=int, required=True, help="border width inside the reserved area before DMZ")
+parser.add_argument("-border", type=int, required=True, help="forbidden border all around inside the reserved area")
 parser.add_argument("-ringwidth", type=int, required=True, help="column width for a ring-oscillator")
 parser.add_argument("-digitheight", type=int, required=True, help="height for the digitizer block")
-parser.add_argument("-hpad", type=int, required=True, help="horizontal padding between rows and colums")
-parser.add_argument("-vpad", type=int, required=True, help="vertical padding between rows and colums")
+parser.add_argument("-hpad", type=int, required=True, help="horizontal padding between ROs")
+parser.add_argument("-vpad", type=int, required=True, help="vertical padding between ROs")
 parser.add_argument("-fmax", required=True, type=float, help="maximum estimated frequency for all ring-oscillators (Hz)")
 parser.add_argument("-len", required=True, type=int, nargs='+', help="number of elements in each ring-oscillator")
 args=parser.parse_args()
@@ -28,25 +28,13 @@ t = len(args.len)-1
 # Command line argument summary
 print("LUTs per item: {:d}".format(args.luts))
 print("Reserved area for rings: (X{:d},Y{:d}) to max (X{:d},Y{:d})".format(args.x, args.y, args.x+args.maxwidth-1, args.y+args.maxheight-1))
-print("Border inside the reserved area before DMZ: {:d}".format(args.border))
+print("Forbidden border inside the reserved area: {:d}".format(args.border))
 print("Padding between rows and colums: ({:d},{:d})".format(args.hpad, args.vpad))
 print("Width for a ring-oscillator: {:d}".format(args.ringwidth))
 print("Height for a digitizer: {:d}".format(args.digitheight))
 print("Number of ring-oscillators: {:d} (T={:d})".format(len(args.len), t))
 print("Lengths of ROs: [{:s}]".format(', '.join(map("{:d}".format, args.len))))
 print("Maximum estimated frequency: {:e} Hz".format(args.fmax))
-
-# Add a reserved area for the digitizer at given position
-def add_digit(x, y, width, height):
-	# Print base info
-	print("- clkdivider_{:02d} origin=({:d},{:d}) size=({:d},{:d})".format(index, x, y, width, height))
-
-	# Return a dictionnary entry for the name and area
-	digit = {}
-	digit['name'] = "digit{:d}".format(index)
-	digit['area'] = {}
-	digit['area']['xilinx'] = xilinx_slice_area(x, y, width, height)
-	return digit
 
 # Add a reserved area for a ring-oscillator at given position
 def add_ring(x, y, width, index, length):
@@ -120,34 +108,32 @@ def xilinx_lut(index):
 	elif index==3:
 		return "D6LUT"
 
-# Start first bank at the left corner of the DMZ area
-x = args.x + args.border
-y = args.y + args.border
-xstart = x
-ystart = y
-xmax = 0
-ymax = 0
+# Prepare an empty dictionnary for the RO calculated parameters
+rings = []
 
-# Prepare an empty dictionnary for the banks calculated parameters
-banks = []
+# Start ring-oscillator bank at the left corner just over the digitizer
+bank_x = args.x + args.border
+bank_y = args.y + args.border + args.digitheight
 
 # For each RO
+x = bank_x
+y = bank_y
+xmax = 0
+ymax = 0
 for index in range(len(args.len)):
 
-	# Create a new dictionnary entry for the current bank
-	bank = {}
-	bank['index'] = index
+	# Create a new dictionnary entry for the current ring
+	ring = {}
+	ring['index'] = index
 
 	# Add the padding for isolation
 	x += args.hpad
 	y += args.vpad
-
-	# Add the digitizer
-	bank['digit'] = add_digit(x, y, args.ringwidth, args.digitheight)
-	y += args.digitheight
+	# if y > bank_y:
+	# 	y += args.vpad
 
 	# Add the ring-oscillator
-	bank['ring'], ringheight = add_ring(x, y, args.ringwidth, index, args.len[index])
+	ring['parameters'], ringheight = add_ring(x, y, args.ringwidth, index, args.len[index])
 	y += ringheight
 	x += args.ringwidth
 
@@ -159,26 +145,40 @@ for index in range(len(args.len)):
 
 	# Check if the next RO will fit in the remaining width in the current row, or start a new row
 	if x+args.hpad+2+args.hpad > args.x+args.maxwidth:
-		x = xstart
+		x = bank_x
 		y = ymax
-		ystart = ymax
 		if index < len(args.len)-1:
 			ymax = 0
 	else:
-		y = ystart
+		y = bank_y
 
 	# Append the bank to the dictionnary
-	banks.append(bank)
+	rings.append(ring)
 
 # Set the final reserved area
 final_width = xmax-args.x+args.hpad+args.border
 final_height = ymax-args.y+args.vpad+args.border
-reserved = xilinx_slice_area(args.x, args.y, final_width, final_height)
+digitalnoise = {}
+digitalnoise['area'] = {}
+digitalnoise['area']['xilinx'] = xilinx_slice_area(args.x, args.y, final_width, final_height)
 
-# Define the DMZ (equals to reserved area without the border)
-dmz = {}
-dmz['area'] = {}
-dmz['area']['xilinx'] = xilinx_slice_area(args.x+args.border, args.y+args.border, final_width-2*args.border, final_height-2*args.border)
+# Set the ROs bank area
+bank_width = final_width - 2*args.border
+bank_height =  final_height - args.digitheight - 2*args.border
+bank = {}
+bank['area'] = {}
+bank['area']['xilinx'] = xilinx_slice_area(bank_x, bank_y, bank_width, bank_height)
+print("- bank origin=({:d},{:d}) size=({:d},{:d})".format(bank_x, bank_y, bank_width, bank_height))
+
+# Set the digitizer area
+digit_x = args.x + args.border
+digit_y = args.y + args.border
+digit_width = final_width - 2*args.border
+digit_height =  args.digitheight
+digitizer = {}
+digitizer['area'] = {}
+digitizer['area']['xilinx'] = xilinx_slice_area(digit_x, digit_y, digit_width, digit_height)
+print("- digitizer origin=({:d},{:d}) size=({:d},{:d})".format(digit_x, digit_y, digit_width, digit_height))
 
 # Init Jijna templating environement
 environment = Environment(loader=FileSystemLoader("templates"))
@@ -187,7 +187,7 @@ environment = Environment(loader=FileSystemLoader("templates"))
 template = environment.get_template("settings.vhd.jinja")
 content = template.render(
 		cmd = cmd,
-		banks = banks
+		rings = rings
 	)
 settings = open("settings.vhd", "w")
 settings.write(content)
@@ -197,9 +197,10 @@ settings.close()
 template = environment.get_template("constraints.xdc.jinja")
 content = template.render(
 		cmd = cmd,
-		reserved = reserved,
-		dmz = dmz,
-		banks = banks
+		digitalnoise = digitalnoise,
+		digitizer = digitizer,
+		bank = bank,
+		rings = rings
 	)
 settings = open("constraints.xdc", "w")
 settings.write(content)
--- hardware/config/digitalnoise/settings.vhd
+++ hardware/config/digitalnoise/settings.vhd
@@ -2,10 +2,19 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 -- This file has been automatically generated with the command line:
--- $ python generate.py -vendor xilinx -luts 4 -x 12 -y 102 -maxwidth 15 -maxheight 22 -border 2 -ringwidth 2 -digitheight 2 -hpad 2 -vpad 2 -fmax 220e6 -len 20 21
+-- $ python generate.py -vendor xilinx -luts 4 -x 12 -y 102 -maxwidth 15 -maxheight 22 -border 2 -ringwidth 2 -digitheight 8 -hpad 2 -vpad 2 -fmax 220e6 -len 20 21
 -- For more information look into the directory 'hardware/config/digitalnoise'.
 package settings is
+
+	-- Ring-oscillators settings
 	constant T: natural := 1;
 	type len_array is array (0 to T) of natural;
 	constant RO_LEN: len_array := (20, 21);
+
+	-- Digitizer settings
+	constant ERO: natural := 1;
+	constant MURO: natural := 2;
+	constant COSO: natural := 3;
+	constant DIGITIZER: natural := COSO;
+
 end package;
