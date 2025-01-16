4978b4e05dcd42a2e250f664c8c2578e3847ca65
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2025-01-16 14:30:05 +0100

    Changing clock divider to pulse (unbalanced duty cycle), setting useful false_path timing contraints

--- hardware/hdl/clkdivider.vhd
+++ hardware/hdl/clkdivider.vhd
@@ -9,53 +9,70 @@ entity clkdivider is
 		FACTOR_WIDTH: natural := 32
 	);
 	port (
+		-- Asynchronous reset
+		reset: in std_logic;
 		-- Input clock to be divided
 		original: in std_logic;
 		-- Division factor (1: no division, 2: division by 2,...)
 		factor: in std_logic_vector (FACTOR_WIDTH-1 downto 0);
+		-- Enable strobed when divisor factor changes
+		changed: in std_logic;
 		-- Divided output clock
 		divided: out std_logic
 	);
 end entity;
 
--- The linear divider can divide the clock by factors in interval [1, 2^FACTOR_WIDTH[.
-architecture rtl of clkdivider is
+-- The pulse divider can divide the clock by factors in interval [1, 2^FACTOR_WIDTH[ with a non balanced duty cycle.
+architecture pulse of clkdivider is
 
-	signal resync: std_logic_vector (FACTOR_WIDTH-1 downto 0) := (others => '0');
-	signal divider: std_logic_vector (FACTOR_WIDTH-1 downto 0) := (others => '0');
-	signal counter: std_logic_vector (FACTOR_WIDTH-1 downto 0) := (others => '0');
-	signal reshaped: std_logic;
+	signal resync, store: std_logic;
+	signal divider: std_logic_vector (FACTOR_WIDTH-1 downto 0);
+	signal counter: std_logic_vector (FACTOR_WIDTH-1 downto 0);
+	signal pulse: std_logic;
 
 begin
 
 	-- Resynchronize the input signal to 'original' clock
-	process (original)
+	process (original, reset)
 	begin
-		if rising_edge(original) then
-			resync <= factor;
-			divider <= resync;
+		if reset = '1' then
+			resync <= '0';
+			store <= '0';
+			divider <= (others => '0');
+		elsif rising_edge(original) then
+			resync <= changed;
+			store <= resync;
+			if store = '1' then
+				divider <= factor;
+			end if;
 		end if;
 	end process;
 
-	-- Reshape a new clock based on counter values ('1111' when counter<factor/2 '0000' when counter>factor/2)
-	process (original)
+	-- Create the pulse when counter is equal to 1
+	process (original, reset)
 	begin
-		if rising_edge(original) then
-			if counter < divider-1 then
+		if reset = '1' then
+			counter <= (others => '0');
+			pulse <= '0';
+		elsif rising_edge(original) then
+			if divider = 2 then
+				counter(0) <= not counter(0);
+				pulse <= counter(0);
+			elsif counter < divider-1 then
 				counter <= counter + 1;
-				if counter > divider(FACTOR_WIDTH-1 downto 1)-1 then
-					reshaped <= '0';
+				if counter = 1 then
+					pulse <= '1';
 				else
-					reshaped <= '1';
+					pulse <= '0';
 				end if;
 			else
 				counter <= (others => '0');
-				reshaped <= '0';
+				pulse <= '0';
 			end if;
 		end if;
 	end process;
 
 	-- Select the rehaped clock if the divided is a least 2, else the original signal
-	divided <= '0' when divider = 0 else original when divider = 1 else counter(0) when divider = 2 else reshaped;
+	divided <= '0' when divider = 0 else original when divider = 1 else pulse;
 
 end architecture;
--- hardware/sim/test_clkdivider/test_clkdivider.py
+++ hardware/sim/test_clkdivider/test_clkdivider.py
@@ -1,12 +1,15 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, RisingEdge, FallingEdge
+import signaltools as Signal
 
 @cocotb.test()
 async def test_division_by_zero(dut):
 	cycles = 1000
 	dut.factor.value = 0
 	await cocotb.start(Clock(dut.original, 10, units="ns").start())
+	await Signal.SetBitDuring(dut.reset, 5, units="ns")
+	await Signal.PulseBit(dut.changed, dut.original)
 	for i in range(cycles):
 		await RisingEdge(dut.original)
 		assert dut.divided.value == 0, "Divided clock output is not tied to logic 0"
@@ -15,8 +18,10 @@ async def do_division_by_n(dut, n):
 	original_period = 10
 	wait = 5
 	cycles = 1000
-	await cocotb.start(Clock(dut.original, original_period, units="ns").start())
 	dut.factor.value = n
+	await cocotb.start(Clock(dut.original, original_period, units="ns").start())
+	await Signal.SetBitDuring(dut.reset, 5, units="ns")
+	await Signal.PulseBit(dut.changed, dut.original)
 	await ClockCycles(dut.divided, wait, rising=True)
 	start_time = cocotb.utils.get_sim_time(units='ns')
 	await ClockCycles(dut.divided, cycles, rising=True)
@@ -47,15 +52,3 @@ async def test_division_by_10(dut):
 @cocotb.test()
 async def test_division_by_100(dut):
 	await do_division_by_n(dut, 100)
-
-@cocotb.test()
-async def test_duty_cycle(dut):
-	cycles = 1000
-	count = [0, 0]
-	dut.factor.value = 5
-	await cocotb.start(Clock(dut.original, 10, units="ns").start())
-	for i in range(cycles):
-		await FallingEdge(dut.original)
-		count[dut.divided.value] += 1
-	duty_cycle = int(100*count[0]/cycles)
-	assert duty_cycle >= 40 and duty_cycle <= 60, "Duty cycle not in range [40, 50]"
