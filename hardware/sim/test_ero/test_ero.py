e5813847600b425b38bd1b872ad841162d2a1c56
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2025-01-16 14:43:46 +0100

    Adding reset signal to ERO (drill to clock divider)

--- hardware/sim/test_ero/test_ero.py
+++ hardware/sim/test_ero/test_ero.py
@@ -5,9 +5,11 @@ import signaltools as Signal
 
 @cocotb.test()
 async def test_gen_random_100(dut):
-	dut.div.value = 1000
+	dut.divider.value = 1000
 	await cocotb.start(Signal.NoisyClock(dut.ro0, 500e6))
 	await cocotb.start(Signal.NoisyClock(dut.ro1, 501e6))
+	await Signal.SetBitDuring(dut.reset, 5, units="ns")
+	await Signal.PulseBit(dut.changed, dut.ro0)
 	for i in range(100):
 		await RisingEdge(dut.clk)
 		dut._log.info("Random bit {:d}".format(dut.data.value.integer))
