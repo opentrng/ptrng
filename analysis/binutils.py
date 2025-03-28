601fe8cdaaaf17a290aa2ac1ba909bb5e4e347ac
PEBAY-PEYROULA Florian <florian.pebay@cea.fr>
2025-03-28 09:15:18 +0100

    Added function to transform an array of bytes to an array of bits

--- analysis/binutils.py
+++ analysis/binutils.py
@@ -14,3 +14,9 @@ def to_words(bits, wordlen):
 	words4bytes = np.packbits(words32bits, axis=1)
 	words = np.apply_along_axis(int.from_bytes, 1, words4bytes, byteorder='big', signed=False)
 	return words
+
+# Expand bytes to an array of bits
+def to_bits(bytes):
+	binary = bin(int.from_bytes(bytes, byteorder='big'))
+	bits = np.array([int(binary[i]) for i in range(2, len(binary))])
+	return bits
